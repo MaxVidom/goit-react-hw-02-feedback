18319d22c9bcdc135eb665fe6044030a12555f49
MaxVidom <MaxVidomenko@gmail.com>
2022-07-14 12:53:04 +0300

    Compleate 4 tasks

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Statistics from './Statistics';
+import Notification from './Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleAddFedback = evt => {
+    const target = evt.currentTarget.name;
+    this.setState(prevState => {
+      return {
+        [target]: prevState[target] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    if (total === 0) {
+      return 'Not enought feedback';
+    }
+    return ((good / total) * 100).toFixed() + '%';
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    return (
+      <div>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleAddFedback}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div>
+      <button type="button" name="good" onClick={onLeaveFeedback}>
+        Good
+      </button>
+      <button type="button" name="neutral" onClick={onLeaveFeedback}>
+        Neutral
+      </button>
+      <button type="button" name="bad" onClick={onLeaveFeedback}>
+        Bad
+      </button>
+    </div>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
